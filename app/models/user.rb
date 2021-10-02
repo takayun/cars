27871547007e620ef6bd0cf2c8375ef0a2796cab
takayun <beachshot10@gmail.com>
2021-10-02 15:02:02 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :shop_name
+    validates :person_name
+    validates :postal_code
+    validates :address
+  end
 end
